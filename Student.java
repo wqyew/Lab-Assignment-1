b1e9ea1fa735872e7ff185c9b4aa6a29202b2cb4
wqyew <122528235+wqyew@users.noreply.github.com>
2023-04-09 11:53:34 +0800

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,82 +1,86 @@
+package Q1;
+
 import java.util.Scanner;
 
-public class Student 
-{
-	private String studentName;
+public class Student {
+	private String name;
 	private char roomType;
-	private double roomRate;
+	private double rate;
+	Scanner input = new Scanner(System.in);
 	
-	public Student()
-	{
-		System.out.println("Welcome to Hostel Fee Calculation Program!");
+	public Student(){
+		System.out.println("\n\tWelcome to Hostel Fee Calculation Program");
 		System.out.println("Room Type available : A, B, C");
-		/*
-		System.out.println(" --------------------------------------- ");
-		System.out.println("|   Room Type   |  Rate (per semester)  |");
-		System.out.println(" --------------------------------------- ");
-		System.out.println("|\tA\t|\tRM 450.00 \t|");
-		System.out.println("|\tB\t|\tRM 400.00 \t|");
-		System.out.println("|\tC\t|\tRM 350.00 \t|");
-		System.out.println(" --------------------------------------- ");
-		*/
-		setDetails(studentName);
+		System.out.println("\t --------------------------------------- ");
+		System.out.println("\t|   ROOM TYPE   |  RATE (per semester)  |");
+		System.out.println("\t --------------------------------------- ");
+		System.out.println("\t|\tA\t|\tRM 450.00 \t|");
+		System.out.println("\t|\tB\t|\tRM 400.00 \t|");
+		System.out.println("\t|\tC\t|\tRM 350.00 \t|");
+		System.out.println("\t --------------------------------------- ");
+		
+		setDetails(name);
 		setRoom(roomType);
 		selectRoom();
 	}
 	
-	public Student(String studentName, char roomType)
-	{
-		this.studentName = studentName;
+	public Student(String name, char roomType){
+		this.name=name;
 		this.roomType = roomType;
 	}
 	
-	public void setDetails(String studentName)
-	{
-		System.out.print("\nPlease enter student name\t: ");
-		Scanner scan = new Scanner(System.in);
-		studentName = scan.nextLine();
-		this.studentName = studentName;
+	public void setDetails(String name) {
+		System.out.print("Please enter your name   : ");
+		name = input.nextLine();
+		for (int i=0; i < name.length() ; i++)
+	        { 
+	            char ch = name.charAt(i);
+	            if(! Character.isLetter(ch) ) {
+	            	System.out.println("Invalid input. Please make sure your input contain letter only!\n");
+	            	System.out.print("Please enter your name   : ");
+	        		name = input.nextLine();
+	            }	   
+      	}    
+		this.name = name;
 	}
 	
-	public void setRoom(char roomType)
-	{
-		System.out.print("Please enter room type (A/B/C)  : ");
-		Scanner scan = new Scanner(System.in);
-		roomType = scan.next().charAt(0);
-		this.roomType = Character.toUpperCase(roomType);
+	public void setRoom(char roomType) {
+		System.out.print("Please enter your choice : ");
+		roomType = input.next().charAt(0);		
+		roomType= Character.toUpperCase(roomType);
+		while(roomType != 'A' && roomType != 'B' && roomType != 'C') {
+			System.out.println("Invalid choice! Please enter again!\n");
+			System.out.print("Please enter your choice : ");
+			roomType = input.next().charAt(0);	
+			roomType= Character.toUpperCase(roomType);
+		}
+		this.roomType = roomType;		
 	}
 	
-	public void selectRoom()
-	{
-		switch(roomType)
-		{
-			case 'A': 
-				roomRate = 450.00;
-				break;
-			
-			case 'B':
-				roomRate = 400.00;
-				break;
-				
-			case 'C':
-				roomRate = 350.00;
-				break;
-				
-			default:
-				System.out.println("Invalid room type!");
-				break;
+
+	public void selectRoom() {
+		if (roomType == 'A'){
+			rate = 450;
+		}
+		
+		else if (roomType == 'B') {
+			rate = 400;
+		}
+		
+		else if (roomType == 'C') {
+			rate = 350;
 		}
 	}
 	
 	public void displayDetails()
 	{
-		System.out.println("\nHere are the details.");
-		System.out.println("Student Name  \t\t : " + studentName.toUpperCase());
+		System.out.println("\nBelow is the report hostel fee:");
+		System.out.println("Student Name  \t\t : " + name.toUpperCase());
 		System.out.println("Room Type   \t\t : " + roomType);
-		System.out.println("Room Rate (per semester) : RM " + roomRate);
-		System.out.println("Room Rate (for one year) : RM " + (roomRate*2));
+		System.out.printf("Room Rate per semester \t : RM %.2f \n", rate);
+		System.out.printf("Room Rate for one year (two semester) : RM %.2f \n", (rate*2));
 	}
-
+	
 	public static void main(String[] args)
 	{
 		Student s1 = new Student();
@@ -84,6 +88,6 @@ public class Student
 		
 		Student s2 = new Student("Sophie", 'A');
 		s2.selectRoom();
-		s2.displayDetails();
+		s2.displayDetails();	
 	}
 }
